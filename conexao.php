afd5f59ac26866c01bda5f894282cc2546cdfdcc
Diogogit1 <101932685+Diogogit1@users.noreply.github.com>
2022-04-18 15:40:08 -0300

    subindo arquivos do projeto

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b9d39380ab098d', '56905ab4', 'heroku_69cf4fe3a461255') or die('Erro ao conectar ao banco de dados');
